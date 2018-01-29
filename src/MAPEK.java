a1c9d9ec0ffec5817ed2687b0693fb5fc23f7953
jeffrey-palmerino <jrp3143@rit.edu>
2018-01-28 20:22:05 -0500

    standard deviation

--- src/MAPEK.java
+++ src/MAPEK.java
@@ -1,5 +1,27 @@
+import java.util.List;
+
+import java.lang.Math;
+
 public class MAPEK{
     public MAPEK(){
         
+    }
+
+    public double getStandardDeviation(List<Integer> latencies){
+        double sd = 0;
+        double mean = 0;
+
+        //calculate mean
+        for(int i=0; i< latencies.size(); i++) {
+            mean = mean + latencies.get(i);
+        }
+        mean = mean / latencies.size();
+
+        //calculate summation
+        for (int i = 0; i < latencies.size(); i++) {
+            sd += Math.pow(latencies.get(i) - mean, 2);
+        }
+
+        return Math.sqrt(sd/(latencies.size()-1));
     }
 }
