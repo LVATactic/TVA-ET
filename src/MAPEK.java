c2bb841abd70322efdfad3b2391814f614fc3cca
jeffrey-palmerino <jrp3143@rit.edu>
2018-01-28 20:49:39 -0500

    standard deviation

--- src/MAPEK.java
+++ src/MAPEK.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import java.lang.Math;
@@ -7,21 +9,42 @@ public class MAPEK{
         
     }
 
-    public double getStandardDeviation(List<Integer> latencies){
-        double sd = 0;
+    public List<Double> getTacticUtility(HashMap<Integer, List<Integer>> map){
+        List<Double> utilities = new ArrayList<>();
+
+        //for each key in hashmap
+        for(Integer key : map.keySet()){
+            List<Integer> latencies = map.get(key);
+
+            double mean = getLatencyMean(latencies);
+            double sd = getStandardDeviation(latencies);
+            double utility = ((14 - mean)*8)/sd;
+
+            utilities.add(utility);
+        }
+
+        return utilities;
+    }
+
+    public double getLatencyMean(List<Integer> latencies){
         double mean = 0;
 
         //calculate mean
         for(int i=0; i< latencies.size(); i++) {
             mean = mean + latencies.get(i);
         }
-        mean = mean / latencies.size();
+        return mean / latencies.size();
+    }
+
+    public double getStandardDeviation(List<Integer> latencies){
+        double sum = 0;
+        double mean = getLatencyMean(latencies);
 
         //calculate summation
         for (int i = 0; i < latencies.size(); i++) {
-            sd += Math.pow(latencies.get(i) - mean, 2);
+            sum += Math.pow(latencies.get(i) - mean, 2);
         }
 
-        return Math.sqrt(sd/(latencies.size()-1));
+        return Math.sqrt(sum/(latencies.size()-1));
     }
 }
