4cfbc958cd6de9c932c5ab261231d0d0ea76c489
Stephen Cioffi <stephen@stephencioffi.com>
2018-01-29 00:13:15 -0500

    Removed Timing mechanism

--- src/Controller.java
+++ src/Controller.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class Controller extends Thread{
+public class Controller{
     private HashMap<Integer, List<Integer>> macro_tactics = new HashMap<Integer, List<Integer>>();
     private int max_iterations;
     private int current_iteration = 0;
@@ -8,22 +8,15 @@ public class Controller extends Thread{
     public Controller(int max_iterations, HashMap<Integer, List<Integer>> macro_tactics){
         this.max_iterations = max_iterations;
         this.macro_tactics = macro_tactics;
-    }
 
-    public void run(){
         while (current_iteration < max_iterations){
-            try {
-                MAPEK mape_k = new MAPEK(macro_tactics);
-                mape_k.runMAPEK();
-                this.sleep(120 * 1000);
-            } catch(InterruptedException e){
-                continue;
-            }
+            MAPEK mape_k = new MAPEK(macro_tactics);
+            mape_k.runMAPEK();
 
             current_iteration++;
         }
     }
-
+    
     public static void main(String[] args){
         HashMap<Integer, List<Integer>> tactics = new HashMap<>();
 
@@ -47,6 +40,6 @@ public class Controller extends Thread{
         list3.add(120);
         tactics.put(2, list3);
 
-        new Thread(new Controller(10, tactics)).start();
+        new Controller(10, tactics);
     }
 }
