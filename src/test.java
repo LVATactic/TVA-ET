b6bf6e4d6ada1709a040bb8305908e91b631aafe
Dan <dxkvse@rit.edu>
2018-01-29 00:22:17 -0500

    Random function

---
+++ src/test.java
@@ -0,0 +1,21 @@
+import java.text.DecimalFormat;
+public class test {
+
+    public static void main(String[] args) {
+        test t = new test();
+        t.run();
+
+    }
+
+
+    private void run(){
+        System.out.println(Round(3.1234534242,5));
+    }
+
+
+    public  double Round(double value, int places) {
+        double scale = Math.pow(10, places);
+        return Math.round(value * scale) / scale;
+    }
+
+}
