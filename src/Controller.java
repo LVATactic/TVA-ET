0fb9910e9c36bab4adb19682e60d93262da62020
Stephen Cioffi <stephen@stephencioffi.com>
2018-01-28 19:54:39 -0500

    Implemented basic Timer with 0 delay to start immediately.

--- src/Controller.java
+++ src/Controller.java
@@ -4,10 +4,16 @@ public class Controller {
     private HashMap<Integer, List<Integer>> macro_tactic = new HashMap<Integer, List<Integer>>();
 
     public Controller(){
+        Timer timer = new Timer();
 
+        timer.schedule( new TimerTask() {
+            public void run() {
+                System.out.println("TIME!");
+            }
+        }, 0, 5*1000);
     }
 
     public static void main(String[] args){
-
+        Controller run = new Controller();
     }
 }
