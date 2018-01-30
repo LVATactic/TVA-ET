874eae26cf02cdfcb6cb2a70665daa33394baa2c
Stephen Cioffi <stephen@stephencioffi.com>
2018-01-29 21:25:17 -0500

    Write diffs to CSV

--- src/Controller.java
+++ src/Controller.java
@@ -13,7 +13,7 @@ public class Controller{
     static final public int MAX_THRESHOLD = 8;
     static public int critical_failure_count = 0;
 
-    public ArrayList<Double> differences;
+    public ArrayList<ArrayList<Double>> differences;
 
 
     public Controller(int max_iterations, HashMap<Integer, List<Integer>> macro_tactics){
@@ -23,9 +23,9 @@ public class Controller{
         ArrayList<ArrayList<Integer>> combined = new ArrayList<>();
 
         combined.add(runInstance(false)); // Baseline
-        combined.add(runInstance(true)); // Purposed algorithm
+        combined.add(runInstance(true)); // Proposed algorithm
 
-        writeToCSV(combined);
+        writeToCSV(combined, differences);
     }
 
     private ArrayList<Integer> runInstance(boolean proposed){
@@ -36,24 +36,28 @@ public class Controller{
         while(cycle < MAX_CYCLES) {
             critical_failure_count = 0;
             current_iteration = 0;
+
+            ArrayList<Double> list_of_differences = new ArrayList<>();
+
+            double latency_difference = 0;
             while (current_iteration < max_iterations) {
                 MAPEK mape_k = new MAPEK(macro_tactics, proposed);
-                double latency_difference = mape_k.runMAPEK(GLOBAL_SYMPTOM);
-                differences.add(latency_difference);
+                latency_difference = mape_k.runMAPEK(GLOBAL_SYMPTOM);
+                list_of_differences.add(latency_difference);
 
                 current_iteration++;
             }
             cycle++;
             critical_failures_list.add(Controller.critical_failure_count);
+            differences.add(list_of_differences);
         }
 
         cycle = 0;
-        System.out.println(differences);
         return critical_failures_list;
     }
 
-    private void writeToCSV(ArrayList<ArrayList<Integer>> lists){
-        String csv_buffer = "Baseline (Critical Failures),Proposed (Critical Failures)\n";
+    private void writeToCSV(ArrayList<ArrayList<Integer>> lists, ArrayList<ArrayList<Double>> differences){
+        String csv_buffer = "Baseline (Critical Failures),TacSim (Critical Failures)\n";
         try{
             FileWriter writer = new FileWriter("output/data.csv");
 
@@ -68,7 +72,23 @@ public class Controller{
             writer.write(csv_buffer);
             writer.close();
 
-        }catch(IOException e){
+
+            
+            FileWriter diff_writer = new FileWriter("output/latency_diffs.csv");
+            String csv_diff_buffer = "Baseline (Latency Diff),TacSim (Latency Diff)\n";
+
+            ArrayList<Double> baseline_diffs = differences.get(0);
+            ArrayList<Double> proposed_diffs = differences.get(1);
+
+            for(int i = 0; i < baseline_diffs.size(); i++){
+                String line = baseline_diffs.get(i) + "," + proposed_diffs.get(i) + "\n";
+                csv_diff_buffer += line;
+            }
+
+            diff_writer.write(csv_diff_buffer);
+            diff_writer.close();
+
+        } catch(IOException e){
             System.err.println("Unable to write CSV file. Please check directory/file permissions.");
         }
     }
